e6a6328857a744dd0ee854cc69870d45c2c71c43
Mehdi Ayache <mehdi@noie.com>
2022-01-29 12:51:20 +0100

    :lipstick: Header

--- components/globalHeader.vue
+++ components/globalHeader.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>Header</div>
+  <div>
+    <div class="c-header">
+      <div class="c-header-logo" />
+      <div class="c-header-info">
+        <div class="c-header-info-first-line">
+          <span>Besoin d'un </span><span>crédit</span
+          ><span> ? Comparez les </span><span>meilleures offres</span
+          ><span> !</span>
+        </div>
+        <div class="c-header-info-second-line">
+          <span
+            >Un crédit vous engage et doit être remboursé. Vérifiez vos
+            capactiés de remboursement avant de vous engager.</span
+          >
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {}
