808770abef9b1bebae043b8a0d329b1fbf55f3ad
Mehdi Ayache <mehdi@noie.com>
2022-01-30 20:45:27 +0100

    :lipstick: Slider

--- components/options.vue
+++ components/options.vue
@@ -6,17 +6,36 @@
       v-model="selectedCategory"
       default="Sélectionnez un type de crédit"
     />
+    <Slider
+      v-model="selectedAmount"
+      :min="0"
+      :max="200000"
+      title="Montant"
+      :format="formatCurrency"
+      type="€"
+    />
+    <Slider
+      v-model="selectedMonths"
+      :min="6"
+      :step="1"
+      :max="120"
+      title="Durée"
+      type=" mois"
+    />
     <Select :items="filters" v-model="selectedFilter" default="Partenaire" />
   </div>
 </template>
 <script>
 import Select from '~/components/select'
+import Slider from '~/components/slider'
 export default {
-  components: { Select },
+  components: { Select, Slider },
   data() {
     return {
       selectedCategory: this.$route.path.slice(1),
       selectedFilter: '',
+      selectedMonths: 36,
+      selectedAmount: 100000,
       filters: [
         { label: 'Montant max', id: 'max-amount' },
         { label: 'Montant min', id: 'min-amount' },
@@ -25,6 +44,11 @@ export default {
         { label: 'Rang', id: 'rank' },
       ],
     }
+  },
+  methods: {
+    formatCurrency(value) {
+      return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+    },
   },
   watch: {
     selectedCategory(value) {
---
+++ components/slider.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="c-slider">
+    <span class="c-slider-title" v-html="computedTitle" />
+    <input
+      type="range"
+      :step="computedStep"
+      :min="min"
+      :max="max"
+      v-model="selectedValue"
+    />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    min: { type: Number, default: 0 },
+    max: { type: Number, default: 100 },
+    step: { type: Number },
+    title: { type: String, default: '' },
+    type: { type: String, default: '' },
+    format: { type: Function, default: (v) => v },
+    value: {
+      type: Number,
+    },
+  },
+  watch: {
+    selectedValue(val) {
+      this.$emit('input', parseInt(val))
+    },
+  },
+  computed: {
+    computedStep() {
+      return this.step || (this.max - this.min) / 100
+    },
+    computedTitle() {
+      return this.title + ': ' + this.format(this.value) + this.type
+    },
+  },
+  data() {
+    return { selectedValue: this.value }
+  },
+}
+</script>
