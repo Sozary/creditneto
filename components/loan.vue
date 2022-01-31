cc017e7f3c898e303dc4854d23a74fc4c1ccb22b
Mehdi Ayache <mehdi@noie.com>
2022-01-31 16:08:22 +0100

    :lipstick: Item

--- components/loan.vue
+++ components/loan.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="c-loan">Salut</div>
+  <div class="c-loan">
+    <span class="c-loan-title"> Annonces: </span>
+    <div class="c-loan-items">
+      <div class="c-loan-items-item">
+        <img src="/assets/images/cetelem.webp" alt="" />
+        <div class="c-loan-items-item-taeg">
+          <span>
+            <strong>TAEG </strong> de <strong>x%</strong> à
+            <strong>x%</strong></span
+          >
+        </div>
+        <div class="c-loan-items-item-example">
+          <span
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+            corporis, distinctio deleniti amet, asperiores delectus labore
+            repellat quis assumenda laborum aliquam optio voluptatibus? Sapiente
+            repudiandae dignissimos, ab iste voluptate possimus!</span
+          >
+        </div>
+        <div class="c-loan-items-item-options">
+          <font-awesome-icon :icon="['fas', 'euro-sign']" />
+          <font-awesome-icon :icon="['fas', 'calendar']" />
+        </div>
+        <div class="c-loan-items-item-data-options">
+          <div class="c-loan-items-item-data-options-amount">
+            <span>Montant : Min </span><span>20 000</span> <span> - Max </span
+            ><span> 1 000 000</span>
+          </div>
+          <div class="c-loan-items-item-data-options-duration">
+            <span>Durée : Min </span><span>36 mois</span><span> - Max </span
+            ><span>420 mois</span>
+          </div>
+        </div>
+        <div class="c-loan-items-item-simulate">
+          <span>simuler</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {}
--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,21 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    [
+      'nuxt-fontawesome',
+      {
+        imports: [
+          {
+            set: '@fortawesome/free-solid-svg-icons',
+            icons: ['fas'],
+          },
+          {
+            set: '@fortawesome/free-brands-svg-icons',
+            icons: ['fab'],
+          },
+        ],
+      },
+    ],
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
   ],
