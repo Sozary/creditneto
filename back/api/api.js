2e757f939d29fbb698a7657dbc45c404b611850e
Mehdi Ayache <mehdi@noie.com>
2022-02-09 10:04:54 +0100

    :hammer: fk

--- back/api/api.js
+++ back/api/api.js
@@ -17,7 +17,7 @@ async function getConnection() {
 function query(connection, query, values) {
   return new Promise((res, rej) => {
     connection.query(query, values, (err, result) => {
-      if (err) rej(`Query error [${query}]: ${error}`)
+      if (err) rej(`Query error [${query}]: ${err}`)
       res(result)
     })
   })
