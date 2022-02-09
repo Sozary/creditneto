80bf9d4415cbb6e22507fd302f27af77c953a1e1
Mehdi Ayache <mehdi@noie.com>
2022-02-09 13:06:12 +0100

    :hammer: client

--- components/loan.vue
+++ components/loan.vue
@@ -63,43 +63,46 @@ export default {
   },
   methods: {
     track(partner) {
-      var _gaq = _gaq || []
-      _gaq.push(['_setAccount', 'UA-219885464-1'])
-      _gaq.push(['_trackPageview'])
-      _gaq.push([
-        '_addTrans',
-        '1234', // order ID - required
-        'Provenance', // affiliation or store name
-        '1.00', // total - required
-        '0', // tax
-        '0', // shipping
-        'Ville', // city
-        'Departement', // state or province
-        'FR', // country
-      ])
+      if (process.client) {
+        var _gaq = _gaq || []
+        _gaq.push(['_setAccount', 'UA-219885464-1'])
+        _gaq.push(['_trackPageview'])
+        _gaq.push([
+          '_addTrans',
+          '1234', // order ID - required
+          'Provenance', // affiliation or store name
+          '1.00', // total - required
+          '0', // tax
+          '0', // shipping
+          'Ville', // city
+          'Departement', // state or province
+          'FR', // country
+        ])
 
-      _gaq.push([
-        '_addItem',
-        '1234', // order ID - required
-        'DD44', // SKU/code - required
-        'CREDIT', // product name
-        'COFIDIS', // category or variation
-        '1.00', // unit price - required
-        '1', // quantity - required
-      ])
-      _gaq.push(['_trackTrans'])(
-        //submits transaction to the Analytics servers
-        function () {
-          var ga = document.createElement('script')
-          ga.type = 'text/javascript'
-          ga.async = true
-          ga.src =
-            ('https:' == document.location.protocol ? 'https://ssl' : '//www') +
-            '.google-analytics.com/ga.js'
-          var s = document.getElementsByTagName('script')[0]
-          s.parentNode.insertBefore(ga, s)
-        }
-      )()
+        _gaq.push([
+          '_addItem',
+          '1234', // order ID - required
+          'DD44', // SKU/code - required
+          'CREDIT', // product name
+          'COFIDIS', // category or variation
+          '1.00', // unit price - required
+          '1', // quantity - required
+        ])
+        _gaq.push(['_trackTrans'])(
+          //submits transaction to the Analytics servers
+          function () {
+            var ga = document.createElement('script')
+            ga.type = 'text/javascript'
+            ga.async = true
+            ga.src =
+              ('https:' == document.location.protocol
+                ? 'https://ssl'
+                : '//www') + '.google-analytics.com/ga.js'
+            var s = document.getElementsByTagName('script')[0]
+            s.parentNode.insertBefore(ga, s)
+          }
+        )()
+      }
       this.$ga.event({
         hitType: 'event',
         eventCategory: 'loan',
