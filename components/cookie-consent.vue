efae8c1805ce8d38ff5faf9d34ab1acaabb78351
Mehdi Ayache <mehdi@noie.com>
2022-03-09 15:54:50 +0100

    :fire: link

--- components/cookie-consent.vue
+++ components/cookie-consent.vue
@@ -1,20 +1,23 @@
 <template>
-  <transition name="fade">
-    <div class="c-cookie" v-if="show">
-      <span class="c-cookie-info"
-        >Ce site utilise des cookies destinés à vous offrir une meilleure
-        expérience utilisateur. En poursuivant votre navigation, vous en
-        acceptez leur utilisation.</span
-      >
-      <div class="c-cookie-ok"><span @click="hideCookieConsent"> ok</span></div>
-      <div class="c-cookie-accept"><span> En savoir plus </span></div>
-    </div>
-  </transition>
+  <div class="c-cookie" v-if="show">
+    <span class="c-cookie-info"
+      >Ce site utilise des cookies destinés à vous offrir une meilleure
+      expérience utilisateur. En poursuivant votre navigation, vous en acceptez
+      leur utilisation.</span
+    >
+    <div class="c-cookie-ok"><span @click="hideCookieConsent"> ok</span></div>
+    <a
+      class="c-cookie-accept"
+      target="_blank"
+      href="https://www.credit-creditneto.com/contact.php"
+      ><span> En savoir plus </span></a
+    >
+  </div>
 </template>
 <script>
 export default {
   data() {
-    return { show: true }
+    return { show: false }
   },
   mounted() {
     if (process.client) {
