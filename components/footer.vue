af17ec2323e7e5838d473660adb86553a3e0f64c
Mehdi Ayache <mehdi@noie.com>
2022-03-30 23:43:58 +0200

    :fire: some changes

--- components/footer.vue
+++ components/footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-footer-bg">
+  <div class="bg-footer-bg mt-2">
     <img
       src="/assets/images/logo-footer.png"
       class="w-36 h-6 ml-[50%] transform -translate-x-1/2 mt-2.5 md:mt-6"
